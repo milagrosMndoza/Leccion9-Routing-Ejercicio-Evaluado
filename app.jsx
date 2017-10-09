f897cfdb12ebdb8d90d69a31275bf884b8982493
milagrosMndoza <milagrosmndoza@gmail.com>
2017-10-09 17:37:28 -0500

    subiendo avance

--- app.jsx
+++ app.jsx
@@ -88,35 +88,35 @@ class Teachers extends React.Component {
 
 class Courses extends React.Component {
 	render() {
-		const {route} = this.props;
+	const {route} = this.props;
     let CurrentList = null;
     let htmlCourse = [
 			{
-				imgSrc: "img/courses/HTML/How.png",
+				img: "img/courses/HTML/How.png",
         		title: "How to Make a Website",
         		content: "If you’ve never built a website before and you have no coding or design experience, this is the place to start. In this project, we learn how to build a modern portfolio website for desktops, tablets, and mobile devices. We start with basic HTML and CSS syntax. Next, we learn how to build custom web pages with an image gallery and contact page. Finally, we walk through how to share a website live on the web."
 			},
 			{
-				imgSrc: "img/courses/HTML/HTMLForms.png",
+				img: "img/courses/HTML/HTMLForms.png",
        			title: "HTML Forms",
       			content:"The web is a two-way communication medium. There’s lots of HTML elements for displaying data and producing output, and conversely, there’s also lots of HTML elements for accepting input. Accepting input from the user means creating web forms. In this course, we’ll learn about all the most important form elements that web professionals use on a daily basis."
 			},
 			{
-				imgSrc: "img/courses/HTML/HTMLVideoyAudio.png",
+				img: "img/courses/HTML/HTMLVideoyAudio.png",
        			title: "HTML Video and Audio",
        			content:"Text and images have always been the foundation of web content, but more than ever, video and audio are also a part of that content mix. Fortunately, we can now create standards-based video and audio players that don't require the use of plugins. Adding video and audio to a webpage is almost as easy as adding an image or formatting some text."
 			},
 			{
-				imgSrc: "img/courses/HTML/SVGBasics.png",
+				img: "img/courses/HTML/SVGBasics.png",
         		title: "SVG Basics",
        			content:"Scalable Vector Graphics (SVG) is an XML markup language for creating two-dimensional images using vectors. This is different from traditional raster-based image formats that use pixels, like JPEG and PNG. When used on web pages, SVG images provide an infinite level of detail, so they look sharp regardless of screen size or pixel density."
         },
         {
-				imgSrc: "img/courses/HTML/ResponsiveImg.png",
+				img: "img/courses/HTML/ResponsiveImg.png",
        			title: "Responsive Images",
       			content:"Using the new source-set and sizes attributes, and the new picture element, it's possible to create images that behave better in a responsive design. These new pieces of markup allow us to deliver the right image to the right device, based on resolution, pixel density, and other factors we define. This will help web pages load faster and look better, and the most capable devices will get the best looking images possible."
 			}, {
-				imgSrc: "img/courses/HTML/IntroHTMLyCSS.png",
+				img: "img/courses/HTML/IntroHTMLyCSS.png",
       			title: "Introduction to HTML and CSS",
       			content:"Get started creating web pages with HTML and CSS, the basic building blocks of web development. HTML, or Hypertext Markup Language, is a standard set of tags you will use to tell the web browser how the content of your web pages and applications are structured. Use CSS, or Cascading Style Sheets, to select HTML tags and tell the browser what your content should look like."
 			}
@@ -124,31 +124,31 @@ class Courses extends React.Component {
 
 	let cssCourse = [
 			{
-				imgSrc: "img/courses/CSS/css1.png",
+				img: "img/courses/CSS/css1.png",
 				title: "CSS Basics",
 				content: "CSS (Cascading Style Sheets) is a style sheet language that describes the presentation of web pages. Whereas HTML is what forms the structure of a web page, CSS is what we use to style the HTML with colors, backgrounds, font sizes, layout, and more. As you'll soon learn, CSS is one of the core technologies for designing and building websites."
 			},
 			{
-				imgSrc: "img/courses/CSS/css2.png",
+				img: "img/courses/CSS/css2.png",
 				title: "CSS Selectors",
 				content:"In this course, we're going to go beyond the basic selector concepts covered in CSS Basics. Besides the common ways to select elements with type, ID and class selectors, we're able to target elements based on their attributes, position in the HTML document, even their relation to other elements."
 			},
 			{
-				imgSrc: "img/courses/CSS/css3.png",
+				img: "img/courses/CSS/css3.png",
 				title: "Responsive Layouts",
 				content:"Responsive web design is a collection of techniques for building websites that work on multiple screen sizes. In these lessons, we're going to use the foundational principles of responsive design as a framework for thinking about page layout. By the end, you should have a better understanding of how to approach common decisions in responsive design."
 			},
 			{
-				imgSrc: "img/courses/CSS/css4.png",
+				img: "img/courses/CSS/css4.png",
 				title: "CSS Flexbox Layout",
 				content:"Flexbox is a set of CSS properties that give you a flexible way to lay out content. With flexbox you can change the direction, alignment, size and order of elements, regardless of their original size and order in the HTML. You can even stretch and shrink elements and distribute space, all with just a few lines of CSS!"
         },
         {
-				imgSrc: "img/courses/CSS/css5.png",
+				img: "img/courses/CSS/css5.png",
 				title: "CSS Transitions and Transforms",
 				content:"CSS transitions and transforms can create simple animations that enhance user interactions in websites and apps. In this course, you'll build an interactive image gallery using CSS transitions and transforms."
 			}, {
-				imgSrc: "img/courses/CSSL/css6.png",
+				img: "img/courses/CSSL/css6.png",
 				title: "Bootstrap 4 Basics",
 				content:"Learn to use Bootstrap 4, one of the most popular open source front end frameworks, to help you build a functional design and layout in little time."
 			}
@@ -156,31 +156,31 @@ class Courses extends React.Component {
 
 	let jsCourse = [
 			{
-				imgSrc: "img/courses/JS/js1.png",
+				img: "img/courses/JS/js1.png",
 				title: "JavaScript Basics",
 				content:"JavaScript is a programming language that drives the web: from front-end user interface design, to backend server-side programming, you'll find JavaScript at every stage of a web site and web application. In this course, you'll learn the fundamental programming concepts and syntax of the JavaScript programming language."
 			},
 			{
-				imgSrc: "img/courses/JS/js2.png",
+				img: "img/courses/JS/js2.png",
 				title: "JavaScript Loops, Arrays and Objects",
 				content:"Storing, tracking and handling data is a large part of computer programming. Arrays provide a method for storing multiple values into a single variable. That makes an array a convenient way to pass around a list of items."
 			},
 			{
-				imgSrc: "img/courses/JS/js3.png",
+				img: "img/courses/JS/js3.png",
 				title: "jQuery Basics",
 				content:"jQuery Basics covers why you'd want to use jQuery, what it is and how to include it in your projects. You'll build several projects over the course to give you the confidence to integrate jQuery in your own projects and add that level of flair and interactivity to any site you work on."
 			},
 			{
-				imgSrc: "img/courses/JS/js4.png",
+				img: "img/courses/JS/js4.png",
 				title: "AJAX Basics",
 				content:"AJAX is an important front-end web technology that lets JavaScript communicate with a web server. It lets you load new content without leaving the current page, creating a better, faster experience for your web site's visitors. In this course, you'll learn how AJAX works and how you can use JavaScript to communicate with a web server."
 			},
         {
-				imgSrc: "img/courses/JS/js5.png",
+				img: "img/courses/JS/js5.png",
 				title: "Interactive Web Pages with JavaScript",
 				content:"In this course we’ll create a to-do list application using JavaScript alone, without using any third party libraries. You’ll get to grips with manipulating and traversing the DOM and adding event handlers to web page elements. The things that you can do with JavaScript are always increasing and this course will give you a solid footing going forward."
 			}, {
-				imgSrc: "img/courses/JS/js6.png",
+				img: "img/courses/JS/js6.png",
 				title: "Node.js Basics",
 				content:"In this course we will create a command line application to retrieve user's profile information from the Treehouse website. We'll be writing our application in JavaScript to run on the Node.js platform."
 			}
@@ -190,17 +190,31 @@ class Courses extends React.Component {
 
 			case 'css':
 				CurrentList = cssCourse.map( (item, index) => {
-					return <li key = {index}> {item.imgSrc} </li>
+					return <li className="row course" key = {index}>
+					<div>
+					<img src={item.img} />
+					<h3>{item.title}</h3>className="row course" 
+					<p>{item.content}</p>
+					</div>
+					 </li>
 				});
 				break;
 			case 'javascript':
 				CurrentList = jsCourse.map( (item, index) => {
-					return <li key = {index}> {item} </li>
+					return <li className="row course" key = {index}> <div>
+					<img src={item.img} />
+					<h3>{item.title}</h3>
+					<p>{item.content}</p>
+					</div> </li>
 				});
 				break;
 			default: //'html'
 				CurrentList = htmlCourse.map( (item, index) => {
-					return <li key = {index}> {item} </li>
+					return <li className="row course" key = {index}> <div>
+					<img src={item.img} />
+					<h3>{item.title}</h3>
+					<p>{item.content}</p>
+					</div>  </li>
 				});
 				break;
 		}
@@ -277,18 +291,18 @@ class App extends React.Component {
          <span class="icn-logo"><i class="material-icons">code</i></span>
             <header>App</header>{' '}
             <menu>
-               <ul >
+               <ul className="main-nav">
                <li>
-                    <a href="#/Home">Home</a>
+			       <a aria-current="true"  className="active" href="#/Home" >Home</a>
                   </li>{' '}
                   <li>
-                     <a href="#/about">About</a>
+                     <a  aria-current="true"  className="active" href="#/about">About</a>
                   </li>{' '}
                   <li>
-                     <a href="#/teachers">Teachers</a>
+                     <a  aria-current="true"  className="active" href="#/teachers">Teachers</a>
                   </li>{' '}
                   <li>
-                     <a href="#/courses">Courses</a>
+                     <a  aria-current="true"  className="active" href="#/courses">Courses</a>
                   </li>
                </ul>{' '}
             </menu>
